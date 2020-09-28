080d4d305732380bb4483ebb5befded1c20f5390
Dave Mateer <davemateer@gmail.com>
2020-09-28 13:51:21 +0100

    auto commit

---
+++ src/CookieDave.Web.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace CookieDave.Web.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(1,1);
+
+        }
+    }
+}
