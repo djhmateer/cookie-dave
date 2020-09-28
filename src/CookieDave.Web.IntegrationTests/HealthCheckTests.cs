4887eb2d0a959de31ed8140b5d4dccb72c72a007
Dave Mateer <davemateer@gmail.com>
2020-09-28 14:28:05 +0100

    auto commit

--- src/CookieDave.Web.IntegrationTests/HealthCheckTests.cs
+++ src/CookieDave.Web.IntegrationTests/HealthCheckTests.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Text;
+﻿using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit;
@@ -27,6 +22,5 @@ namespace CookieDave.Web.IntegrationTests
             //Assert.Equal(HttpStatusCode.OK, response.StatusCode);
             response.EnsureSuccessStatusCode();
         }
-        
     }
 }
