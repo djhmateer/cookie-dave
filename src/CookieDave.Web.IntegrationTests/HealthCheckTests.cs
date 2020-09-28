80f29d038d8d33c4fc89cd9d311a0ee96408c5bf
Dave Mateer <davemateer@gmail.com>
2020-09-28 14:27:01 +0100

    auto commit

---
+++ src/CookieDave.Web.IntegrationTests/HealthCheckTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace CookieDave.Web.IntegrationTests
+{
+    public class HealthCheckTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient _httpClient;
+
+        public HealthCheckTests(WebApplicationFactory<Startup> factory)
+        {
+            _httpClient = factory.CreateDefaultClient();
+        }
+
+        [Fact]
+        public async Task HealthCheck_ReturnsOk()
+        {
+            var response = await _httpClient.GetAsync("/healthcheck");
+
+            //Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            response.EnsureSuccessStatusCode();
+        }
+        
+    }
+}
