6005ce67a7b59e8e8b13faa7fdc3e0663e4cb096
Dave Mateer <davemateer@gmail.com>
2020-09-28 13:22:04 +0100

    auto commit

--- src/CookieDave.Web/Pages/Crawl.cshtml.cs
+++ src/CookieDave.Web/Pages/Crawl.cshtml.cs
@@ -1,6 +1,5 @@
 ﻿using System.Security.Claims;
 using CookieDave.Web.Data;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using static CookieDave.Web.Data.CDRole;
 
@@ -9,23 +8,17 @@ namespace CookieDave.Web.Pages
     [AuthorizeRoles(Tier1, Tier2, Admin)]
     public class CrawlModel : PageModel
     {
-        private readonly IHttpContextAccessor httpContextAccessor;
-        public CrawlModel(IHttpContextAccessor httpContextAccessor) => this.httpContextAccessor = httpContextAccessor;
-
         public string? Message { get; set; }
 
         public void OnGet()
         {
-            var name = httpContextAccessor.HttpContext.User.Identity.Name;
+            var roleClaims = User.FindAll(ClaimTypes.Role);
 
-            Message = "roles: ";
-            var claims = httpContextAccessor.HttpContext.User.FindAll(ClaimTypes.Role);
-            foreach (var claim in claims)
+            Message = "Role claims are: ";
+            foreach (var claim in roleClaims)
             {
-                var role = claim.Value;
-                Message += role + " ";
+                Message += claim.Value + " ";
             }
         }
     }
-
 }
